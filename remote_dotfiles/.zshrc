6db4d0845c2bc202721bd1b275c0f6378d361d56
kenf1 <103538771+kenf1@users.noreply.github.com>
2025-07-26 22:55:27 -0700

    Set neovim default

--- remote_dotfiles/.zshrc
+++ remote_dotfiles/.zshrc
@@ -1,1 +1,3 @@
 eval "$(oh-my-posh init zsh --config $HOME/.config/ohmyposh/zen.toml)"
+export EDITOR="nvim"
+export VISUAL="nvim"
--- scripts/initial_setup.sh
+++ scripts/initial_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install)
-sudo nix run nix-darwin --extra-experimental-features "nix-command flakes" -- switch --flake ~/nix#devm
+sudo nix run nix-darwin --extra-experimental-features \
+    "nix-command flakes" -- switch --flake ~/nix#base
